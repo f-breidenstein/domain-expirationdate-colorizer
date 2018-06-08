d7c5c00cb9eff64c0e527181d0b46c182ebb4776
Felix Breidenstein <mail@felixbreidenstein.de>
2018-06-08 13:13:38 +0200

    Versionbump to 1.1
    - Fixed bug with string to date conversion
    - Removed all debug console.log
    - Fixed logic in the if/else block

--- inwx.js
+++ inwx.js
@@ -2,18 +2,20 @@ function endsWith(str, suffix) {
   return str.indexOf(suffix, str.length - suffix.length) !== -1;
 }
 
-function createDatetimeFromString( string ){
-  // "27.09.2018 21:59"
+function createDatetimeFromString(string) {
+  // Original format is "27.09.2018 21:59"
   var arr = string.split(" ");
   var date = arr[0].split(".");
   var time = arr[1].split(":");
-  return new Date(date[2], date[1] , date[0] , time[0] , time[1])
+  // Date() expects "monthIndex" to start at 0 (January)
+  // Therefore we need to substracts 1
+  return new Date(date[2], date[1] - 1, date[0], time[0], time[1])
 }
 
 var currentDate = new Date();
 var okColor = "green";
 
-var warnLimit = 42  * 24 * 60 * 60 * 1000;
+var warnLimit = 42 * 24 * 60 * 60 * 1000;
 var warnColor = "yellow";
 
 var criticalLimit = 14 * 24 * 60 * 60 * 1000;
@@ -27,28 +29,20 @@ var middleTable = document.getElementById("tbl_middle_table");
 var allTd = middleTable.getElementsByClassName("dl_td");
 
 // filter the 'td' to find the ones representing the expiration dates
-var arr = Array.prototype.slice.call( allTd )
-arr.forEach(function callback(item){
-  if (endsWith(item.id, "_exDate")){
+var arr = Array.prototype.slice.call(allTd)
+arr.forEach(function callback(item) {
+  if (endsWith(item.id, "_exDate")) {
 
     var innerDiv = item.children[0];
-    console.log(innerDiv.textContent);
     var expDate = createDatetimeFromString(innerDiv.textContent);
-    console.log(expDate);
-    console.log(currentDate);
     var diff = expDate - currentDate;
-    console.log(diff);
-    console.log(warnLimit);
-    console.log(criticalLimit);
 
-    if(diff < warnLimit){
-      console.log("warn");
-      innerDiv.style.backgroundColor = warnColor;
-    } else if (diff < criticalLimit){
-      console.log("crit");
+    if (diff < criticalLimit) {
+      innerDiv.style.color = "white";
       innerDiv.style.backgroundColor = criticalColor;
+    } else if (diff < warnLimit) {
+      innerDiv.style.backgroundColor = warnColor;
     } else {
-      console.log("ok");
       innerDiv.style.backgroundColor = okColor;
       innerDiv.style.color = "white";
     }
